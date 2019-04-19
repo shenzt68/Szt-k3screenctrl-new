f042736cdf46f92ce533aa83f8de385eab02dd23
zxlhhyccc <45259624+zxlhhyccc@users.noreply.github.com>
2019-04-19 22:32:15 +0800

    Update signals.c

--- src/signals.c
+++ src/signals.c
@@ -42,9 +42,9 @@ void refresh_screen_timeout() { g_last_check_time = time(NULL); }
 
 static void check_screen_timeout() {
     if (CFG->screen_timeout != 0 &&
-        time(NULL) - g_last_check_time >= CFG->screen_timeout && g_is_screen_on == 1 ){
+        time(NULL) - g_last_check_time >= CFG->screen_timeout) {
+        extern int g_is_screen_on;
         g_is_screen_on = 0; /* Do not process key messages - just wake up if there are any */
-        	    page_switch_to(PAGE_BASIC_INFO);//page wan have a bug
         request_notify_event(EVENT_SLEEP);
     }
 }
